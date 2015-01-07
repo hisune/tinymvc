1fb97e1b41dc50c283cfae66ffbfb3f157ef348c
hisune <hisunecom@gmail.com>
2015-01-07 23:07:05 +0800

    ip bug
    ip bug

--- Tiny/UserAgent.php
+++ Tiny/UserAgent.php
@@ -143,16 +143,7 @@ class UserAgent
      *  */
     public static function ip()
     {
-        if (getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
-            $ip = getenv('HTTP_CLIENT_IP');
-        } elseif (getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
-            $ip = getenv('HTTP_X_FORWARDED_FOR');
-        } elseif (getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
-            $ip = getenv('REMOTE_ADDR');
-        } elseif (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-        return preg_match('/[\d\.]{7,15}/', $ip, $matches) ? $matches [0] : '';
+        return preg_match('/[\d\.]{7,15}/', $_SERVER['REMOTE_ADDR'], $matches) ? $matches [0] : '';
     }
 
     private static function _userAgentMatch($ua, $tokens){
